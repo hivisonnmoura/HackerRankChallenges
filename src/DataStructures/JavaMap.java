a0d315100e1860b641fac4b496d6b7cec7183e28
hivisonnmoura <hivison.moura@gmail.com>
2017-07-04 08:37:01 -0300

    Java Map challenge.

---
+++ src/DataStructures/JavaMap.java
@@ -0,0 +1,33 @@
+package DataStructures;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by Hivison N Moura on 04/07/2017.
+ */
+public class JavaMap {
+
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        Map<String, Integer> phoneBook = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            int phone = in.nextInt();
+            in.nextLine();
+            phoneBook.put(name, phone);
+        }
+        while (in.hasNext()) {
+            String s = in.nextLine();
+            if(phoneBook.containsKey(s)){
+                System.out.println(s+"="+phoneBook.get(s));
+            }else{
+                System.out.println("Not found");
+            }
+        }
+    }
+}
+
